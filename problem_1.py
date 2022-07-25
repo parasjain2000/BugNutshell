10b35f51249ce9fd26d53e4ea4e046fb0512c876
Sumanth Sureshkumar <slattshoes@gmail.com>
2022-07-25 14:30:54 -0700

    more efficient solution

--- problem_1.py
+++ problem_1.py
@@ -1,99 +1,54 @@
 import re
-#Regex pattern for cdets
-pattern = re.compile("CSC[a-zA-Z][a-zA-Z]([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?")
-
-#opening & reading in the branch, and split html file into list
-file = open("v21.20.x-prs.html","r")
-text = str(file.read()).split()
-
-#should add user input here
-build_1 = str(85903)
-build_2 = str(85786)
+pattern = re.compile("CSC[a-zA-Z][a-zA-Z]\d\d\d\d\d\s")
 
 
-b1_idx = 0
-b2_idx = 0
+#If a user inputs an invalid build number, the function will just return "No CDETs present between both builds."
+def find_cdets(build_1,build_2):
+    b1_idx = 0
+    b2_idx = 0
+    b1_cdets = []
+    b2_cdets = []
+    with open("v21.20.x-prs.html") as fp:
+        if build_1 != build_2:
+            for i, line in enumerate(fp):
+                if build_1 in line:
+                    b1_idx = i
+                    break
+            for i, line in enumerate(fp,start=b1_idx+1):
+                mo = pattern.match(line)
+                if mo != None:
+                    b1_cdets.append(str(mo.group(0)).strip())
+                if build_2 in line:
+                    b2_idx = i
+                    break
+            for i, line in enumerate(fp,start=b2_idx+1):
+                mo = pattern.match(line)
+                if mo != None:
+                    b2_cdets.append(str(mo.group(0)).strip())
+                if "=====" in line:
+                    break
+        else:
+            for i, line in enumerate(fp):
+                if build_1 in line:
+                    b1_idx = i
+                    break
+            for i, line in enumerate(fp,start=b1_idx+1):
+                mo = pattern.match(line)
+                if mo != None:
+                    b1_cdets.append(str(mo.group(0)).strip())
+                if "=====" in line:
+                    break
+    fp.close()
 
-try:
-    #Locate where the build number is in the list
-    b1_idx = text.index(build_1)
-    b2_idx = text.index(build_2)
-#if invalid build number is input
-except ValueError:
-    
-    print("One or more build numbers are invalid.")
-
-
-b1_end_idx = 0
-b2_end_idx = 0
-b1_cdets = []
-b2_cdets = []
-
-
-
-#interate from build number #1 index to end, break when five equal characters are found, as that indicates we are going past the current build number
-for i in range(b1_idx + 2,len(text)):
-    if text[i] == "=====":
-        b1_end_idx = i
-        break
-
-#iterate from build number #1 index to the index found previously
-for i in range(b1_idx+2,b1_end_idx):
-    if bool(re.match(pattern,text[i])):
-        b1_cdets.append(text[i])
-
-#interate from build number #2 index to end, break when five equal characters are found, as that indicates we are going past the current build number
-for i in range(b2_idx + 2,len(text)):
-    if text[i] == "=====":
-        b2_end_idx = i
-        break
-
-#iterate from build number #2 index to the index found previously
-for i in range(b2_idx+2,b2_end_idx):
-    if bool(re.match(pattern,text[i])):
-        b2_cdets.append(text[i])
-
-#find possible duplicate cdets. This will take care of scenario where user enters two same build numbers
-duplicates = set(b1_cdets).intersection(b2_cdets)
-isEmpty = (len(duplicates) == 0)
-
-if isEmpty:
-    print("No CDETs present between both builds.")
-else:
-    print(duplicates)
+    duplicates = set(b1_cdets).intersection(b2_cdets)
+    if build_1 == build_2:
+        duplicates = b1_cdets
+    isEmpty = (len(duplicates) == 0)
+    if isEmpty:
+        return "No CDETs present between both builds."
+    else:
+        return duplicates
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#
+print(find_cdets("85489","85188"))
