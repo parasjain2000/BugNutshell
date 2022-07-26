e3b6a36449c0e558ef148bb2f43010516f9453a9
Sumanth Sureshkumar <slattshoes@gmail.com>
2022-07-25 18:18:11 -0700

    added "reverse" logic

--- problem_1.py
+++ problem_1.py
@@ -1,13 +1,9 @@
 import re
 pattern = re.compile("CSC[a-zA-Z][a-zA-Z]\d\d\d\d\d\s")
 
-
-#If a user inputs an invalid build number, the function will just return "No CDETs present between both builds."
 def find_cdets(build_1,build_2,file_name):
     b1_idx = 0
-    b2_idx = 0
     b1_cdets = []
-    b2_cdets = []
     with open(file_name) as fp:
         if build_1 != build_2:
             for i, line in enumerate(fp):
@@ -20,12 +16,6 @@ def find_cdets(build_1,build_2,file_name):
                     b1_cdets.append(str(mo.group(0)).strip())
                 if build_2 in line:
                     b2_idx = i
-                    break
-            for i, line in enumerate(fp,start=b2_idx+1):
-                mo = pattern.match(line)
-                if mo != None:
-                    b2_cdets.append(str(mo.group(0)).strip())
-                if "=====" in line:
                     break
         else:
             for i, line in enumerate(fp):
@@ -40,15 +30,17 @@ def find_cdets(build_1,build_2,file_name):
                     break
     fp.close()
 
-    duplicates = set(b1_cdets).intersection(b2_cdets)
-    if build_1 == build_2:
-        duplicates = b1_cdets
-    isEmpty = (len(duplicates) == 0)
-    if isEmpty:
-        return "No CDETs present between both builds."
+    if len(b1_cdets) == 0:
+        return "One or more build numbers may be invalid"
     else:
-        return duplicates
+        return b1_cdets
 
 
-#
-print(find_cdets("85489","85188"))
+build_1 = 78834
+build_2 = 78864
+file_name = "v21.20.x-prs.html"
+
+if build_1 > build_2:
+    print(find_cdets(str(build_1),str(build_2),file_name))
+else:
+    print(find_cdets(str(build_2),str(build_1),file_name))
