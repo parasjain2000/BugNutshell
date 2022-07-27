5f37462884463cff2b0aa243c47e26c854125315
Sumanth Sureshkumar <slattshoes@gmail.com>
2022-07-27 12:51:04 -0700

    Create problem_3.py

---
+++ problem_3.py
@@ -0,0 +1,24 @@
+
+def find_branch_and_commit_id(file_name):
+    dict = {}
+    with open (file_name) as fp:
+        for i, line in enumerate(fp):
+            if "CSCvv" in line and "boxer" in line and "mergedtobranch" in line:
+                title = line.split(" ")[0]
+                title_s = title.split(".")
+                dots = title.count('.')
+                commit_id = title_s[dots].replace("ascii","")
+                branch = title_s[3]
+                hi = title_s[dots-1]
+                for i in range(4,dots):
+                    branch = branch + "." + title_s[i]
+
+                dict[commit_id] = branch
+    fp.close()
+    return dict
+
+
+print(find_branch_and_commit_id("CSCvv01962.txt"))
+
+
+
